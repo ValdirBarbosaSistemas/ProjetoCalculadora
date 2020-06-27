30acda3961048a3af85962bf0dbb6aca484a3207
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-06-27 17:46:00 -0300

    Modificando a classe Calculadora

--- ProjetoCalculadoraUdemy/src/main/java/br/com/cod3r/calc/visao/Calculadora.java
+++ ProjetoCalculadoraUdemy/src/main/java/br/com/cod3r/calc/visao/Calculadora.java
@@ -1,21 +1,37 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Calculadora extends JFrame {
 
     //Construtor
     public Calculadora() {
-        setVisible(true);
+        
+        organizarLayout();
+        
+        setSize(232, 322);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        //setPreferredSize(null);
-        setSize(233, 322);
+        setVisible(true);
         //Configurações da tela
 
     }
-
+    
     public static void main(String[] args) {
         new Calculadora(); //Chamando o construtor
+    }
+
+    //Métodos
+    private void organizarLayout() {
+        setLayout(new BorderLayout());
+        
+        Display display = new Display();
+        display.setPreferredSize(new Dimension(233, 60));
+        add(display, BorderLayout.NORTH);
+        
+        Teclado teclado = new Teclado();
+        add(teclado, BorderLayout.CENTER);
     }
 }
