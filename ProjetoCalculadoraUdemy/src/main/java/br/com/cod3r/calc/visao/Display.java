27da7ad1865d13fc5d7590d5c390ed6018dce76a
Valdir Barbosa da Silva Junior <valdir.barbosasistemas@gmail.com>
2020-06-28 12:14:17 -0300

    Mudanças nas Classes
    Criações de alguns métodos nas classes Bota, Display e Teclado

--- ProjetoCalculadoraUdemy/src/main/java/br/com/cod3r/calc/visao/Display.java
+++ ProjetoCalculadoraUdemy/src/main/java/br/com/cod3r/calc/visao/Display.java
@@ -8,17 +8,16 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
-    
-    private final JLabel label;
 
-    //Construtor
-    public Display() {
-        setBackground(new Color(46, 49, 50));
-        label = new JLabel("1234,56"); //Chamando pelo construtor
-        add(label);
-        label.setForeground(Color.WHITE);
-        label.setFont(new Font("courier", Font.PLAIN, 30));
-        
-        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
-    }
+	private final JLabel label;
+
+	// Construtor
+	public Display() {
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("1234,56"); // Chamando pelo construtor
+		add(label);
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+	}
 }
