090c5a75224b84c12af2e012ece37929ae48aee5
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-06-28 11:39:22 -0300

    Mudanças na Classe Botao

--- ProjetoCalculadoraUdemy/src/main/java/br/com/cod3r/calc/visao/Botao.java
+++ ProjetoCalculadoraUdemy/src/main/java/br/com/cod3r/calc/visao/Botao.java
@@ -1,8 +1,15 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Font;
+
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton {
-
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setFont(new Font("courier", Font.PLAIN, 25));
+    }
 }
